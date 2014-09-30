7f25d0d58d257e2f4e91625eee8da97d17833035
gavlu <gavinlue@gmail.com>
2014-09-30 17:52:27 -0400

    Changed the index.js to work using sequalize and to use the real database see.sql. Also added a favicon and updated all of the routers to also use the database file.

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,21 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   var tweets = models.Tweet.findAll({include:[models.User]}).success(function(tweets) {
-    // console.log(tweets);
     res.render('index', { title: 'Twitter.js', tweets: tweets });
   });
-  // var tweets = store.list();
 });
 
 router.post('/submit', function(req, res) {
   var name = req.body.name;
   var text = req.body.text;
-  var id = ; //FILL IN
-  store.push(name, text, id); //PUSH NEW TWEET
-  io.sockets.emit("new_tweet", { name: name, text: text, id: id });
-  res.redirect('/');
+
+  models.User.findOrCreate({where: {name: name}}).complete(function(err, user) {
+    models.Tweet.create({UserId: user[0].id, tweet: text}).complete(function(err, data) {
+      io.sockets.emit("new_tweet", { name: name, text: text});
+      res.redirect('/');
+    });
+  });
+
 });
 
 module.exports = router;
